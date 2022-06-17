a39a80d95797fb26944c61b54b4edf70efdf36d8
Oluwashina Aladejubelo <oluwashina.aladejubelo@gopuff.com>
2022-06-17 14:47:07 +0100

    Initial update

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, jsonify
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from requests import request
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from flask_restful import Api, Resource
 from werkzeug.exceptions import HTTPException
 from werkzeug.exceptions import default_exceptions
 import settings
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.errorhandler(Exception)
@@ -15,23 +18,27 @@ def handle_error(e):
         code = e.code
     return jsonify(error=str(e)), code
 
+
 for ex in default_exceptions:
     app.register_error_handler(ex, handle_error)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+app.config[
+    'SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
 app.config['BUNDLE_ERRORS'] = settings.BUNDLE_ERRORS
 
-db = SQLAlchemy(app)
 api = Api(app)
 api.prefix = '/api'
 
-from endpoints.users.resource import UsersResource
-from endpoints.todos.resource import TodosResource
 
-api.add_resource(UsersResource, '/users', '/users/<int:user_id>')
-api.add_resource(TodosResource, '/todos', '/todos/<int:todo_id>')
+class Message(Resource):
+
+    @cross_origin()
+    def get(self):
+        return {"message": request.args.get("msg")}
+
+
+api.add_resource(Message, "/")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
